a4cd986a9b782cfbb16717dd6ceb9c59810dcdce
Igor Pomozenkov <work-server1@mail.ru>
2021-07-13 22:57:20 +0700

    task completed

---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,31 @@
+<template>
+    <div >
+        <select v-model="selected">
+            <option v-for="option in categories" :key="option" :value="option">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategorySelect',
+    props: {
+        categories: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data(){
+        return {
+            selected: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" module>
+
+
+</style>
