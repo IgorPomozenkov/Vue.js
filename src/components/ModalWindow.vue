247cebb8349988be4ad20502df6896c7325ff71c
Igor Pomozenkov <work-server1@mail.ru>
2021-07-30 22:55:52 +0700

    task completed

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="[$style.wrapper]">
+        <ul :class="[$style.contextMenu]">
+            <li>Редактировать</li>
+            <li>Удалить</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalWindow',
+}
+</script>
+
+<style lang="scss" module>
+.wrapper {
+    position: absolute;
+    top: 40px;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 20px 2px 0;
+    background: silver;
+    z-index: 1;
+}
+
+.contextMenu {
+    list-style-type: none;
+}
+
+.contextMenu li {
+    margin-bottom: 7px;
+    cursor: pointer;
+}
+
+.contextMenu li:hover {
+    text-decoration: underline;
+}
+</style>
